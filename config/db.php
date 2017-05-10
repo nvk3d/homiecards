ccb322c6b34efc1c23d602803a36fb3defeac494
nvk3d <nike_bondar@mail.ru>
2017-05-10 21:13:46 +0300

    changed db connect

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=homiecards',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=fool',
     'username' => 'root',
     'password' => 'naked_rus',
     'charset' => 'utf8',
