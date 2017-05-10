ce977d0a6c768670fef5956c00abf803d8f8189a
nvk3d <nike_bondar@mail.ru>
2017-05-10 21:47:42 +0300

    del db.php file

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=homiecards',
-    'username' => 'root',
-    'password' => 'naked_rus',
-    'charset' => 'utf8',
-];
